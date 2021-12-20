35f66f58a01a41582d42a222dd045976740f9f2b
unknown <amanullahsayeem@gmail.com>
2021-12-20 21:21:18 +0600

    first

--- rozzva/settings.py
+++ rozzva/settings.py
@@ -139,16 +139,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
-
-
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = ""
+MEDIA_ROOT = os.path.join(BASE_DIR, 'Media')
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
-MEDIA_ROOT = os.path.join(BASE_DIR, "Media")
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
